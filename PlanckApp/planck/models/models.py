4af143ce12c34b3d9342a0743fd2585bd4f46bdf
root <root@ELLISR-LT.test.com>
2017-08-31 14:57:20 -0700

    Added UI with ability to classify a requested image with requested weights

--- PlanckApp/planck/models/models.py
+++ PlanckApp/planck/models/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Image(models.Model):
+    name = models.CharField(max_length=100)
+
+class Result(models.Model):
+    result = models.CharField(max_length=100)
+    probability = models.FloatField()
+    output = models.IntegerField()
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+
--- PlanckApp/planck/models/urls.py
+++ PlanckApp/planck/models/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     #results page
     url(r'^classify', views.classify, name='classify'),
+    #page for updating the model
+    url(r'^train',    views.train,   name='train'),
 ]
